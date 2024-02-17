915f3256b5a0ef16d4f2c8dc30e8a4c24009cc7c
YoshinoriKamimoto <k.yoshinori0210@gmail.com>
2024-02-18 02:54:00 +0900

    first-commit

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿// ストラテジーデザインパターンサンプルプログラム
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // 使用するアルゴリズム
+        IAlgorithm algorithm = new Algorithm1();
+        // IAlgorithm algorithm = new Algorithm2();
+
+        // アルゴリズム実行
+        new AlgorithmExecuter(algorithm).DoProcess();
+    }
+}
+
+// インターフェース
+public interface IAlgorithm
+{
+    public void Process();
+}
+
+// アルゴリズム実行クラス
+public class AlgorithmExecuter
+{
+    private IAlgorithm algorithm;
+    public AlgorithmExecuter(IAlgorithm algorithm)
+    {
+        this.algorithm = algorithm;
+    }
+
+    public void DoProcess()
+    {
+        this.algorithm.Process();
+    }
+}
+
+// 具体的な処理を定義するクラス
+// アルゴリズム1
+public class Algorithm1 : IAlgorithm
+{
+    public void Process()
+    {
+        Console.WriteLine("A");
+    }
+}
+
+// アルゴリズム2
+public class Algorithm2 : IAlgorithm
+{
+    public void Process()
+    {
+        Console.WriteLine("B");
+    }
+}
